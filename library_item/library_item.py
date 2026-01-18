2ca3a8e089f2b03063d82b4971e72260aefec0fc
sukhmani25 <ssukhmani@academic.rrc.ca>
2026-01-18 15:19:46 -0600

    Added accessors and documentation to LibraryItem class

--- library_item/library_item.py
+++ library_item/library_item.py
@@ -53,4 +53,49 @@ class LibraryItem:
             self.__is_borrowed = is_borrowed
         else:
             raise ValueError("Is Borrowed must be a boolean value.")
+    
+    @property
+    def title(self) -> str:
+        """
+        Accessor for title attribute
+        Returns:
+            str: title
+        """
+        return self.__title
+    
+    @property
+    def author(self) -> str:
+        """
+        Accessor for author attribute
+        Returns:
+            str: author
+        """
+        return self.__author
+    
+    @property
+    def genre(self) -> Genre:
+        """
+        Accessor for genre attribute
+        Returns:
+            str: genre
+        """
+        return self.__genre
+    
+    @property
+    def item_id(self) -> int:
+        """
+        Accessor for item_id attribute
+        Returns:
+            item_id: An id number to uniquely identify the library item.
+        """
+        return self.__item_id
+    
+    @property
+    def is_borrowed(self) -> bool:
+        """
+        Accessor for is_borrowed attribute
+        Returns:
+            is_borrowed: Identifies whether the library item is borrowed (True) or available (False).
+        """
+        return self.__is_borrowed
         
